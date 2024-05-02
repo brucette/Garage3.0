53a383fa151471ea64745ab38c15db24ad0cd073
Tina Bruce <tina@fishface.se>
2024-05-02 11:42:16 +0200

    Added Member entity

---
+++ Garage3.0/Entites/Member.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage3._0.Entites
+{
+    public class Member
+    {
+        [Key]
+        public string MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public bool Membership { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        // something here e.g. 
+        // public ICollection<Ownership> Ownerships { get; set; }
+    }
+}
