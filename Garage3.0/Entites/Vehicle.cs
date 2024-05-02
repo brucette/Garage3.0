7c79fa6d69d26f8c0eb99972e2e7e27aa6a756ed
PatrikHogblom <patrik.hogblom@live.se>
2024-05-02 11:17:34 +0200

    added Fordon Entity

---
+++ Garage3.0/Entites/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage3._0.Entites
+{
+    public class Vehicle
+    {
+        public string RegisterNumber { get; set; }
+        public string Color { get; set; }
+        public string ModelType { get; set; }
+        public string Brand { get; set; }
+        public int MyProperty { get; set; }
+        //public int VehicleTypeId { get; set; }
+        //public VehicleType VehicleType { get; set; }
+    }
+}
