26d17b8c6ecb83ab5609baaac95ff7b42eed87b1
Solange Mukamurenzi <murenzisoso@yahoo.com>
2024-05-02 12:03:11 +0200

    Receipt

---
+++ Garage3.0/Entites/Receipt.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Garage3._0.Entites
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+        public DateTime? CheckoutTime { get; set; }
+
+        public TimeSpan? TotalParkingTime { get; set; }
+        public double Price { get; set; }
+
+        // Foregin key
+        //[ForeignKey("RegisterNumber")]
+        //public string RegisterNumberEntity { get; set; }
+    }
+}
