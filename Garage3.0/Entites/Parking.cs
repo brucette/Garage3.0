c6a6aa67a864b0932a2de1f4116203bc69b30261
PatrikHogblom <patrik.hogblom@live.se>
2024-05-02 13:33:11 +0200

    added parking entity

---
+++ Garage3.0/Entites/Parking.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage3._0.Entites
+{
+    public class Parking
+    {
+        public int Id { get; set; }
+        public int ParkingLotNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        //forEGIN Key
+        //public string RegisterNumberEntity { get; set; }    
+    }
+}
