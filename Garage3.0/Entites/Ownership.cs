3c9e3b09ed823eb8515686684dfe1272277a29d8
FelixEdenborgh <Felix@edenborgh.se>
2024-05-02 11:23:56 +0200

    Ownership added

---
+++ Garage3.0/Entites/Ownership.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage3._0.Entites
+{
+    public class Ownership
+    {
+        // Foregin key
+        public string PersonNumber { get; set; }
+        // Foregin key
+        public string RegisterNumber { get; set; }
+
+    }
+}
