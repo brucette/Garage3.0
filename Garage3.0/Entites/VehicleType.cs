c2199ec5968e58535785a32c01a66aa68f7792a7
PatrikHogblom <patrik.hogblom@live.se>
2024-05-02 11:21:34 +0200

    added VehicleType class

---
+++ Garage3.0/Entites/VehicleType.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage3._0.Entites
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int NumWheels { get; set; }
+        
+        //Foregin Key
+        //public int RegisterNumber { get; set; }
+
+    }
+}
