108276d2de66a0f090fb14f9915c8d26ed58f1a1
PatrikHogblom <patrik.hogblom@live.se>
2024-05-07 16:00:38 +0200

    can now show receipt after deleting a parked car, receipt information is stored in database table receipts and is shown in a site.

---
+++ Garage3.0/ModelView/ReceiptViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Garage3._0.ModelView
+{
+    public class ReceiptViewModel
+    {
+        public int Id { get; private set; }
+        public string PersonNumber { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int VehicleType { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public TimeSpan TotalParkingTimeInMinutes { get; set; }
+        public double Price { get; set; }
+    }
+}
