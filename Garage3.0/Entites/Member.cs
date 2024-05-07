0c6859127249923605c2a7abda6cf907b40cb11c
FelixEdenborgh <Felix@edenborgh.se>
2024-05-07 10:54:15 +0200

    La till valid check på firstname och lastname, så att de inte är samma. la också till att den alltid blir toUpper så att det inte går att ändra om i bokstäverna utan att den får ändå fel om det är samma.

--- Garage3.0/Entites/Member.cs
+++ Garage3.0/Entites/Member.cs
@@ -8,12 +8,13 @@ namespace Garage3._0.Entites
         [Required(ErrorMessage = "Social number is required")]
         public string Id { get; set; } // here we will input personNumber
 
-        [Required]
-        [MaxLength(50)]
+
+        [Required(ErrorMessage = "First Name is required")]
+        [UniqueName(ErrorMessage = "First Name and Last Name cannot be the same.")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "Last Name is required")]
+        [UniqueName(ErrorMessage = "First Name and Last Name cannot be the same.")]
         public string LastName { get; set; }
 
         public bool Membership { get; set; }
@@ -24,3 +25,4 @@ namespace Garage3._0.Entites
         public ICollection<Ownership>? Ownerships { get; set; }//Declare this null we don't require ownership to create a new member 
     }
 }
+
---
+++ Garage3.0/Entites/UniqueNameAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage3._0.Entites
+{
+    public class UniqueNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var member = (Member)validationContext.ObjectInstance;
+
+            // Kontrollera om FirstName och LastName är samma
+            if (member.FirstName.ToUpper() == member.LastName.ToUpper())
+            {
+                return new ValidationResult("First Name and Last Name cannot be the same.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
